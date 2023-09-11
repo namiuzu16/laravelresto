721e264cd0f4767d7f7b3ad736fc3aeca850a416
namiuzu16 <namiuzunafa@gmail.com>
2023-09-11 13:38:02 +0700

    first commit

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'idorderdetail',
+        'idorder',
+        'idmenu',
+        'jumlah',
+        'hargajual',
+        'status',
+    ];
+   
+}
